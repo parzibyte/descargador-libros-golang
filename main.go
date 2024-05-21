b87018caad6424acf838d49694fd29f849e0fbc8
Luis Cabrera Benito <parzibyte@gmail.com>
2024-05-21 11:19:38 -0600

    Permitir descarga de libros históricos y agregando menú

--- main.go
+++ main.go
@@ -13,6 +13,26 @@ import (
 	"github.com/signintech/gopdf"
 )
 
+const TIPO_HISTORICO = "historico"
+const TIPO_NORMAL = "normal"
+const ORIENTACION_VERTICAL = "v"
+const ORIENTACION_HORIZONTAL = "h"
+
+var expresionRegularLibroActual = regexp.MustCompile(`(?m)https://libros\.conaliteg\.gob\.mx/(\d+)/\w+\.html?`)
+var expresionRegularLibroHistorico = regexp.MustCompile(`(?m)https://historico\.conaliteg\.gob\.mx/([a-zA-Z0-9]+)\.html?`)
+
+func obtenerTipoDeLibro(urlLibro string) (string, error) {
+	coincidencias := expresionRegularLibroHistorico.FindStringSubmatch(urlLibro)
+	if len(coincidencias) == 2 {
+		return TIPO_HISTORICO, nil
+	}
+
+	if len(expresionRegularLibroActual.FindStringSubmatch(urlLibro)) == 2 {
+		return TIPO_NORMAL, nil
+	}
+	return "", fmt.Errorf("tipo de libro desconocido")
+}
+
 func obtenerCodigoFuenteDeVisualizadorDeLibro(urlLibro string) (string, error) {
 	respuesta, err := http.Get(urlLibro)
 	if err != nil {
@@ -31,15 +51,14 @@ func obtenerCodigoFuenteDeVisualizadorDeLibro(urlLibro string) (string, error) {
 }
 
 func extraerAñoDeLibroSegunUrl(urlLibro string) (string, error) {
-	expresionRegularPaginas := regexp.MustCompile(`(?m)https://libros\.conaliteg\.gob\.mx/(\d+)/\w+\.html?`)
-	coincidenciasPagina := expresionRegularPaginas.FindStringSubmatch(urlLibro)
+	coincidenciasPagina := expresionRegularLibroActual.FindStringSubmatch(urlLibro)
 	if len(coincidenciasPagina) < 2 {
 		return "", fmt.Errorf("no se pudo extraer el año del libro. ¿Es una URL correcta? debe tener la forma https://libros.conaliteg.gob.mx/AÑO/CLAVE.htm. Coincidencias: %v", coincidenciasPagina)
 	}
 	return coincidenciasPagina[1], nil
 }
 
-func extraerClaveAñoYPaginas(urlLibro string) (claveLibro string, año string, cantidadDePaginas int, err error) {
+func extraerClaveAñoYPaginas(urlLibro string, tipo string) (claveLibro string, año string, cantidadDePaginas int, err error) {
 	codigoFuente, err := obtenerCodigoFuenteDeVisualizadorDeLibro(urlLibro)
 	if err != nil {
 		return
@@ -62,7 +81,9 @@ func extraerClaveAñoYPaginas(urlLibro string) (claveLibro string, año string, 
 		return
 	}
 	claveLibro = coincidenciasClave[1]
-	año, err = extraerAñoDeLibroSegunUrl(urlLibro)
+	if tipo == TIPO_NORMAL {
+		año, err = extraerAñoDeLibroSegunUrl(urlLibro)
+	}
 	return
 }
 
@@ -77,8 +98,8 @@ func descargarImagenDeInternetYDevolverReader(url string) (io.Reader, error) {
 	return respuesta.Body, nil
 }
 
-func descargarLibro(urlLibro string) error {
-	claveLibro, año, cantidadPaginas, err := extraerClaveAñoYPaginas(urlLibro)
+func descargarLibro(urlLibro string, tipo string, orientacion string) error {
+	claveLibro, año, cantidadPaginas, err := extraerClaveAñoYPaginas(urlLibro, tipo)
 	if err != nil {
 		return err
 	}
@@ -89,7 +110,12 @@ Cantidad de páginas: %d
 `,
 		urlLibro, claveLibro, año, cantidadPaginas)
 	pdf := gopdf.GoPdf{}
-	tamañoDePagina := *gopdf.PageSizeA4
+	var tamañoDePagina gopdf.Rect
+	if orientacion == ORIENTACION_VERTICAL {
+		tamañoDePagina = *gopdf.PageSizeA4
+	} else {
+		tamañoDePagina = *gopdf.PageSizeA4Landscape
+	}
 	pdf.Start(gopdf.Config{PageSize: tamañoDePagina})
 	var url string
 	/*
@@ -110,10 +136,17 @@ Cantidad de páginas: %d
 			https://libros.conaliteg.gob.mx/2023/c/P1LPM/000.jpg
 		*/
 		numeroDePaginaConCeros := fmt.Sprintf("%0*d", cantidadDeDigitosQueConformanLaCantidadDePaginas, numeroDePaginaActual)
-		url = fmt.Sprintf(
-			"https://libros.conaliteg.gob.mx/%s/c/%s/%s.jpg",
-			año, claveLibro, numeroDePaginaConCeros,
-		)
+		if tipo == TIPO_NORMAL {
+			url = fmt.Sprintf(
+				"https://libros.conaliteg.gob.mx/%s/c/%s/%s.jpg",
+				año, claveLibro, numeroDePaginaConCeros,
+			)
+		} else {
+			url = fmt.Sprintf(
+				"https://historico.conaliteg.gob.mx/c/%s/%s.jpg",
+				claveLibro, numeroDePaginaConCeros,
+			)
+		}
 		fmt.Printf("Descargando imagen %d/%d que se encuentra en %s...", numeroDePaginaActual+1, cantidadPaginas, url)
 		lectorRespuestaHttp, err := descargarImagenDeInternetYDevolverReader(url)
 		fmt.Printf("OK\nAgregando imagen descargada a PDF...")
@@ -141,15 +174,38 @@ Cantidad de páginas: %d
 }
 
 func main() {
-	fmt.Printf(`Ingresa la URL del libro. Puede tener la forma 1 o 2. Después de haberlo ingresado, presiona Enter.\n
-URL del libro: `)
+	fmt.Printf(`
+Parzibyte's blog
+https://parzibyte.me/blog
+
+Descargador de libros
+
+
+Ingresa la URL del libro. Puede tener una de las siguientes formas:
+1: https://historico.conaliteg.gob.mx/CLAVE.htm
+2: https://libros.conaliteg.gob.mx/AÑO/CLAVE.htm
+
+Escribe o pega la URL del libro SIN ESPACIOS y presiona ENTER: `)
 	var urlLibro string
+	var orientacion string = "v"
 	fmt.Scanln(&urlLibro)
-	err := descargarLibro(urlLibro)
+	tipo, err := obtenerTipoDeLibro(urlLibro)
 	if err != nil {
-		fmt.Printf("Error descargando libro. El error es: %v. Recuerde: la URL debe tener el formato https://libros.conaliteg.gob.mx/AÑO/CLAVE.htm por ejemplo https://libros.conaliteg.gob.mx/2023/P1LPM.htm",
+		fmt.Printf("Error obteniendo tipo de libro o tipo de libro inválido: %v", err)
+	}
+	fmt.Printf("Elige la orientación. Escribe v para que sea vertical o h para que sea horizontal. Escribe la letra y presiona ENTER. Orientación[v/h]: ")
+	fmt.Scanln(&orientacion)
+	if orientacion != ORIENTACION_HORIZONTAL && orientacion != ORIENTACION_VERTICAL {
+		fmt.Printf("Error: la orientación debe ser v o h, pero escribiste '%s'", orientacion)
+		return
+	}
+	err = descargarLibro(urlLibro, tipo, orientacion)
+	if err != nil {
+		fmt.Printf("Error descargando libro. El error es: %v",
 			err)
 	} else {
-		fmt.Printf("\n\nLibro descargado correctamente en el mismo lugar donde se encuentra este programa")
+		fmt.Printf("\n\nLibro descargado correctamente en el mismo lugar donde se encuentra este programa\n")
 	}
+	fmt.Printf("Puedes volver a ejecutar este programa cuando quieras descargar otro libro. Presiona ENTER para salir")
+	fmt.Scanln(&orientacion)
 }
